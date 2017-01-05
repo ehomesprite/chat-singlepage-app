30d086a66583b5b2aa533045136683b368182aa2
snowdrop <ehome_sprite@hotmail.com>
2017-01-06 01:34:20 +0800

    firefox view compatibility fix
    sign(

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ var mainVm = new Vue({
 	methods:{
 		send:function () {
 			socket.emit('chat message',$("#sendText").val());
-			//console.log($("#sendText").val());
 		}
 	}
 });
@@ -14,5 +13,4 @@ var mainVm = new Vue({
 var socket = io('http://45.76.149.222:3000/');
 socket.on('chat message', function(msg){
 	mainVm.msg.push(msg);
-	//console.log(typeof msg);
 });
