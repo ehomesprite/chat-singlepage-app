0eaa46433aff5e318e5e65eb77d1a13bdc025bd8
snowdrop <ehome_sprite@hotmail.com>
2017-01-06 01:22:51 +0800

    add socket.io-sendMessage
    add socket.io-sendMessage

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ var mainVm = new Vue({
 	el: "#main",
 	data:{
 		msg:[],
+	},
+	methods:{
+		send:function () {
+			socket.emit('chat message',$("#sendText").val());
+			//console.log($("#sendText").val());
+		}
 	}
 });
 
-var socket = io();
+var socket = io('http://45.76.149.222:3000/');
 socket.on('chat message', function(msg){
-	main.msg.push(msg);
+	mainVm.msg.push(msg);
+	//console.log(typeof msg);
 });
