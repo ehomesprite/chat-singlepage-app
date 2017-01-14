4028c3bec6dd2f88e9428b46c6eeaf9b1af0c9de
snowdrop <ehome_sprite@hotmail.com>
2017-01-14 15:55:59 +0800

    update
    add :  userlist , username
    fix:    css , Vue.mainVM

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+var headerVm = new Vue({
+	el: "#header",
+	data: setting,
+});
+
 var mainVm = new Vue({
 	el: "#main",
 	data:{
 		msg:[],
+		userlist:[],
 	},
 	methods:{
 		send:function () {
 			socket.emit('message',$("#sendText").val());
 			var msgPack = {
 				msg:$("#sendText").val(),
-				self:true
+				self:true,
+				username:setting.username,
 			}
 			this.msg.push(msgPack);
 		}
@@ -20,13 +27,23 @@ socket.on('message', function(msgPack){
 	msgPack.self = false;
 	mainVm.msg.push(msgPack);
 });
+socket.on('userList', function(userlist){
+	//mainVm.userlist = userlist;
+	console.log(userlist);
+});
+
 
 $.ajax({
 	type:'post',
 	url:setting.tokenUrl,
 	async:true,
 	success:function(data){
-		setting.userID = data.uid;//这边在传token的时候也顺手吧uid传过来吧  省事
-		socket.emit('signIn',data.token);
+		if(data.error == 100){
+			location.href = "login.html";
+		}else{
+			setting.userID = data.uid;//这边在传token的时候也顺手吧uid传过来吧  省事
+			setting.userName = data.username;
+			socket.emit('signIn',data.token);
+		}
 	}
 });
