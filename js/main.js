692617826e2812c2ef8fd6becf3609d2253b126a
snowdrop <ehome_sprite@hotmail.com>
2017-01-18 22:32:01 +0800

    update function
    update: emojiList and message reprocess

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ var mainVm = new Vue({
 	data:{
 		msg:[],
 		userlist:[],
+		emojiList:[],
+		emojiListShow:false,
 	},
 	methods:{
 		send:function (event) {
@@ -19,46 +21,106 @@ var mainVm = new Vue({
 					username:setting.username,
 				}
 				this.msg.push(msgPack);
-			}else{
+			}else{// such as '/roll'
 				socket.emit('console',$("#sendText").val().slice(1));
 			}
 			$("#sendText").val("");
-		}
-	}
-});
-
-var socket = io(setting.socketUrl);
-socket.on('message', function(msgPack){
-	//msgPack.self = false;
-	mainVm.msg.push(msgPack);
-});
-socket.on('console',function(msgPack){
-	msgPack.isConsole = true;
-	mainVm.msg.push(msgPack);
-})
-socket.on('userList', function(userlist){
-	userlist = JSON.parse(userlist);
-	console.log(userlist);
-	mainVm.userlist = userlist;
-});
-
-
-$.ajax({
-	type:'post',
-	url:setting.tokenUrl,
-	async:true,
-	success:function(data){
-		if(data.error == undefined){
-			setting.userID = data.uid;//这边在传token的时候也顺手吧uid传过来吧  省事
-			setting.username = data.username;
-			socket.emit('signIn',data.token);
-		}else if (data.error == 100){
-			location.href = "../users/login";
-		}else{
-			location.href = "../users/login";
-		}
+		},
+		emojiInsert:function(category,name){
+			$("#sendText")[0].value += "!:" + category + "/" + name + ":!";
+			$("#sendText").focus();
+		},
+		emojiListTrigger:function(){
+			if(!this.emojiListShow){
+				this.emojiListShow = true;
+			}else{
+				this.emojiListShow = false;
+			}
+		},
 	},
-	error:function(){
-		location.href = "../users/login";
+	watch:{
+		emojiList:function(){
+			setTimeout(function(){
+				$(".emojiList .nav-tabs a:first").tab('show');
+			});
+		},
+		msg:function(){
+			setTimeout(function(){
+				var reg = /!:[\w\/]+:!/img;
+				$(".messageText").each(function(){
+					if($(this).html().match(reg)!=-1){
+						var inner = $(this).html().replace(reg,function(str){
+							var _src= 'img/'+str.slice(2,-2)+'.png';
+							return "<img class='emoji' src='"+_src+"'/>";
+						});
+						$(this).html(inner);
+					}
+				});
+			});
+		},
 	}
 });
+
+function emojiInit () {
+	$.ajax({
+		type:'post',
+		url:setting.emojiUrl,
+		async: true,
+		success:function(data){
+			if(data!=undefined){
+				mainVm.emojiList = data;
+			}
+		}
+	})
+}
+emojiInit();
+
+socket:{
+	var socket = io(setting.socketUrl);
+	socket.on('message', function(msgPack){
+		mainVm.msg.push(msgPack);
+	});
+	socket.on('console',function(msgPack){
+		msgPack.isConsole = true;
+		mainVm.msg.push(msgPack);
+	})
+	socket.on('userList', function(userlist){
+		userlist = JSON.parse(userlist);
+		console.log(userlist);
+		mainVm.userlist = userlist;
+	});
+}
+
+function loginCheck () {
+	$.ajax({
+		type:'post',
+		url:setting.tokenUrl,
+		async:true,
+		success:function(data){
+			if(data.error == undefined){
+				setting.userID = data.uid;//这边在传token的时候也顺手吧uid传过来吧  省事
+				setting.username = data.username;
+				socket.emit('signIn',data.token);
+			}else if (data.error == 100){
+				location.href = "../users/login";
+			}else{
+				location.href = "../users/login";
+			}
+		},
+		error:function(){
+			location.href = "../users/login";
+		}
+	});
+}
+loginCheck();
+
+emojiList_close:{
+	$("body").bind('click',function(){
+		if($(this).parents(".inputWindow").length == 0){
+			mainVm.emojiListShow = false;
+		}
+	});
+	$(".inputWindow").click(function(){
+		return false;
+	});
+}
--- js/setting.js
+++ js/setting.js
@@ -1,5 +1,6 @@
 setting={
 	socketUrl : "/snowchat",
 	tokenUrl : "/apis/snowchat/token",
+	emojiUrl : "/apis/snowchat/emoji",
 	username : "test",
 };
